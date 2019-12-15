5be249c357af33fa96138d9c4f85b2f2677429dd
ryanvilbrandt <ryan.v@zonarsystems.com>
2019-12-14 17:58:05 -0800

    Added root index redirect

--- comic_cozy/urls.py
+++ comic_cozy/urls.py
@@ -17,8 +17,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("ComicCozy/", include("ComicCozy.urls")),
+    path("", RedirectView.as_view(url="ComicCozy")),
+    path("ComicCozy/", include("ComicCozy.urls"), name="ComicCozy"),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
